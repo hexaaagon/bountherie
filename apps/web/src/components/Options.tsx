ea48b7c36ba68b416ae62f20e1436a2a7649f515
Hexaa <me@hexagonn.my.id>
2026-07-11 22:01:01 +0700

    feat(options): added image-based boundaries options

--- apps/web/src/components/Options.tsx
+++ apps/web/src/components/Options.tsx
@@ -1,54 +1,56 @@
-import { PanoramaIcon, VideoIcon } from "@phosphor-icons/react";
 import { cva } from "class-variance-authority";
 import { useState } from "react";
-import { cn } from "@/lib/utils";
-
-export type OptionsType = {
-  mediaType: /* "none", */ "static" | "animated";
-  boundaries: "none" | "cut" | "clean";
-};
 
 export const buttonOption = cva(
-  "flex flex-col justify-center items-center gap-2 py-4 w-48 border rounded-[1rem] transition",
+  "transition-all duration-300",
   {
     variants: {
       variant: {
-        default: "bg-accent/20",
-        picked: "bg-accent-foreground border-accent/80 text-background",
+        default: "invert-100 dark:invert-0",
+        picked: "invert-0 dark:invert-100",
       },
     },
   },
 );
 
-export default function Options() {
-  const [options, setOptions] = useState<OptionsType>({ mediaType: "static", boundaries: "none" });
+export interface OptionsType {
+  boundaries: "none" | "cut" | "clean" | "clear";
+};
+
+export interface OptionsProps {
+  name: string;
+  id: Exclude<OptionsType["boundaries"], "none">;
+  image: {
+    src: string;
+    attributes: React.ImgHTMLAttributes<HTMLImageElement>;
+  }
+}
+
+export default function Options({ options: args }: { options: OptionsProps[] }) {
+  const [options, setOptions] = useState<OptionsType>({ boundaries: "none" });
 
   return (
     <main className="border-foreground/10 border-x px-4 mx-auto max-w-4xl relative">
       <h2 className="font-medium text-sm absolute top-2 right-2 opacity-50">unfinished ui/ux</h2>
-      <section className="flex flex-col w-full pt-8 pb-4 items-center gap-4">
-        <h2 className="font-medium text-3xl">Boundaries</h2>
-        <div className="grid grid-cols-2 gap-4">
-          <button
-            type="button"
-            className={buttonOption({
-              variant: options.mediaType === "static" ? "picked" : "default",
-            })}
-            onClick={() => setOptions((opt) => ({ ...opt, mediaType: "static" }))}
-          >
-            <PanoramaIcon size={48} />
-            <p className="text-center text-xl font-medium">Cut</p>
-          </button>
-          <button
-            type="button"
-            className={buttonOption({
-              variant: options.mediaType === "animated" ? "picked" : "default",
-            })}
-            onClick={() => setOptions((opt) => ({ ...opt, mediaType: "animated" }))}
-          >
-            <VideoIcon size={48} />
-            <p className="text-center text-xl font-medium">Clean</p>
-          </button>
+      <section className="flex flex-col w-full tracking-tighter pt-8 pb-4 px-4 items-start gap-4">
+        <h2 className="font-semibold text-4xl">Boundaries</h2>
+        <div className="grid grid-cols-3 gap-4">
+          {args.map((opt, idx) => (
+            <button
+              key={opt.id}
+              type="button"
+              className={buttonOption({
+                variant: options.boundaries === opt.id ? "picked" : "default",
+              })}
+              onClick={() => setOptions((oldOption) => ({ ...oldOption, boundaries: opt.id }))}
+            >
+              <img
+                src={opt.image.src}
+                {...opt.image.attributes}
+                alt="Optimized Carousel Asset"
+              />
+            </button>
+          ))}
         </div>
       </section>
     </main>
